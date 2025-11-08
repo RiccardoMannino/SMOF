9c76ca88ae98df910e9db151aa579ec1a074955c
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2025-11-08 15:20:51 +0100

    prova chiavi env

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,12 +1,12 @@
 export const apiVersion = "2025-05-16";
 
 export const dataset = assertValue(
-	process.env.NEXT_PUBLIC_SANITY_DATASET,
+	process.env.SANITY_STUDIO_DATASET,
 	"Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
 );
 
 export const projectId = assertValue(
-	process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+	process.env.SANITY_STUDIO_PROJECT_ID,
 	"Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
