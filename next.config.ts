046423ebf7c902db86ecf2837a8868463b63be2e
riccardo mannino <manninoriccardo3@gmail.com>
2026-05-29 11:26:41 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,26 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-	turbopack: {
-		root: path.join(__dirname, ".."),
-	},
-	images: {
-		qualities: [60, 70, 90],
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "cdn.sanity.io",
-			},
-			{
-				protocol: "https",
-				hostname: "img.icons8.com",
-			},
-			{
-				protocol: "https",
-				hostname: "*.googleusercontent.com",
-			},
-		],
-	},
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
+  images: {
+    qualities: [60, 70, 90],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
